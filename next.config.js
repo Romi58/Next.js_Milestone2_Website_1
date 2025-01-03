9d7f6fd4c1603614533e6124b3ebde92ae4b0cac
Romi58 <romaisanaz67@gmail.com>
2025-01-03 22:22:20 +0500

    uploading next.config.js

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'redribbonbakeshop.com',
+      'encrypted-tbn0.gstatic.com',
+      'i.ytimg.com',
+      'pastrieslikeapro.com',
+    ],
+  },
+};
+
+module.exports = nextConfig;
+
+
